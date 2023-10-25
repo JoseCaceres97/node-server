059701feb91e0bfaf65117a9b60f32e81efc6d80
JoseCaceres97 <josemiguelcaceresg@gmail.com>
2023-10-25 16:53:36 -0500

    Promesas añadidas a las funciones

--- app.js
+++ app.js
@@ -20,93 +20,112 @@ function menu() {
 
 //1. AÑADIR TAREA
 const addTask = (task) => {
-  taskList.push(task);
-  console.log(`| La tarea: '${task}', ha sido añadida con exito. |`);
-  console.log("");
+  return new Promise((resolve) => {
+    taskList.push(task);
+    console.log(`| La tarea: '${task}', ha sido añadida con éxito. |`);
+    console.log("");
+    resolve(task);
+  });
 };
 
-//2. ELIMINAR TAREA
 const deleteTask = (index) => {
-  console.log("");
-  if (index >= 0 && index < taskList.length) {
-    const deletedTask = taskList.splice(index, 1);
-    console.log(`| Tarea eliminada. ${deletedTask}: |`);
-  } else {
-    console.log(`< La tarea no existe. >`);
-  }
+  return new Promise((resolve, reject) => {
+    console.log("");
+    if (index >= 0 && index < taskList.length) {
+      const deletedTask = taskList.splice(index, 1);
+      console.log(`| Tarea eliminada: ${deletedTask} |`);
+      resolve(deletedTask);
+    } else {
+      console.log(`< La tarea no existe. >`);
+      reject('Tarea no encontrada');
+    }
+  });
 };
+
 
 //3. COMPLETAR TAREA
-function completeTask(index) {
-  console.log("");
-  if (index >= 0 && index < taskList.length) {
-    taskList[index] = taskList[index] + " | Tarea Completada ✓ | ";
-    console.log(" | Tarea Completada | ");
-  } else {
-    console.log(`< Acción invalida. >`);
-    return false;
-  }
-}
+const completeTask = (index) => {
+  return new Promise((resolve, reject) => {
+    console.log("");
+    if (index >= 0 && index < taskList.length) {
+      taskList[index] = taskList[index] + " | Tarea Completada ✓ |";
+      console.log(" | Tarea Completada |");
+      resolve(taskList[index]);
+    } else {
+      console.log(`< Acción inválida. >`);
+      reject('Acción inválida');
+    }
+  });
+};
+
 //4. LISTAR TAREAS
 function showTasks() {
-  console.log("");
-  console.log("| Lista de Tareas |");
-  console.log("");
-  for (let i = 0; i < taskList.length; i++) {
-    console.log(`${i + 1}. ${taskList[i]}`);
-  }
+  return new Promise((resolve, reject) => {
+    console.log("");
+    console.log("| Lista de Tareas |");
+    console.log("");
+    for (let i = 0; i < taskList.length; i++) {
+      console.log(`${i + 1}. ${taskList[i]}`);
+    }
+    resolve(); // Resolvemos la promesa cuando se completan las tareas
+  });
 }
 
 //5. MOSTRAR MENU POR CONSOLA
-function showMenu() {
+async function showMenu() {
   console.log(menu());
-  leerDatos.question("Introduzca la opción deseada: ", (opcion) => {
-    console.log("");
-    switch (opcion) {
-      case "1":
-        leerDatos.question("-Escribe una tarea:  ", (task) => {
-          console.log("");
-          addTask(task);
-          showMenu();
-        });
-        break;
-      case "2":
+  const opcion = await new Promise((resolve) => {
+    leerDatos.question("Introduzca la opción deseada: ", resolve);
+  });
+
+  console.log("");
+  switch (opcion) {
+    case "1":
+      const task = await new Promise((resolve) => {
+        leerDatos.question("-Escribe una tarea:  ", resolve);
+      });
+      console.log("");
+      await addTask(task);
+      showMenu();
+      break;
+    case "2":
+      const taskIdToDelete = await new Promise((resolve) => {
         leerDatos.question(
           "Ingresa el Id de la tarea que deseas eliminar:  ",
-          (task) => {
-            console.log("");
-            deleteTask(parseInt(task, 10) - 1);
-            showMenu();
-          }
+          resolve
         );
-        break;
-      case "3":
+      });
+      console.log("");
+      await deleteTask(parseInt(taskIdToDelete, 10) - 1);
+      showMenu();
+      break;
+    case "3":
+      const taskIdToComplete = await new Promise((resolve) => {
         leerDatos.question(
           "Ingresa el Id de la tarea que deseas completar:  ",
-          (task) => {
-            console.log("");
-            completeTask(parseInt(task, 10) - 1);
-            showMenu();
-          }
+          resolve
         );
-        break;
-      case "4":
-        console.log("");
-        showTasks();
-        showMenu();
-        break;
-      case "5":
-        console.log("Saliendo del programa.");
-        leerDatos.close;
-        break;
-      default:
-        console.log(
-          "Opcion no valida. Por favor seleccione una opcion valida."
-        );
-        showMenu();
-        break;
-    }
-  });
+      });
+      console.log("");
+      await completeTask(parseInt(taskIdToComplete, 10) - 1);
+      showMenu();
+      break;
+    case "4":
+      console.log("");
+      showTasks();
+      showMenu();
+      break;
+    case "5":
+      console.log("Saliendo del programa.");
+      leerDatos.close();
+      break;
+    default:
+      console.log(
+        "Opción no válida. Por favor seleccione una opción válida."
+      );
+      showMenu();
+      break;
+  }
 }
 
 showMenu();
